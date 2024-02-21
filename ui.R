3e86e24e235619e8be7b95da9b944939564993c6
kregtt <jean-luc.lheveder@insee.fr>
2024-02-21 13:33:26 +0000

    20240221 test5

--- ui.R
+++ ui.R
@@ -1,4 +1,19 @@
-
+library(aws.s3)
+library(shiny)
+library(shinyjs)
+library(lubridate)
+library(dplyr)
+library(tableHTML)
+library(stringi)
+library(stringr)
+library(tidyr)
+library(writexl)
+library(rmarkdown)
+library(grid)
+library(gridExtra)
+library(openxlsx)
+library(knitr)
+library(png)
 
 
 ui <- fluidPage(
